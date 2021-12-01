cb48b97e0dcc0f8db6f49a0d76d87a9e463171bd
gabrielligoski <gabriel.ligoski@gmail.com>
2021-12-01 11:45:03 -0300

    Better text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
+import logo from "./img/logo2.png";
 
 function App() {
     return (
         <div className="wrapper w-full h-full">
+            <img src={logo} className="w-48 my-6" alt="logo"/>
             <div className="container">
                 <div className="animated-title">
-                    <div className="text-top text-4xl">
-                        <div>
+                    <div className="text-top text-4xl text-right">
+                        <div className="w-full">
                             <span>Você deu mais um passo em direcão ao sucesso</span>
                             <span>Bem vindo ao CIEE</span>
                         </div>
                     </div>
                     <div className="text-bottom text-3xl">
-                        <div>“Se a oportunidade não bater, construa uma porta”, Milton Berle</div>
+                        <div>“Se a oportunidade não bater, construa uma porta”,<br/> Milton Berle</div>
                     </div>
                 </div>
             </div>
