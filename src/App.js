48462ef550393757811cef06e734312c6ee6ce4d
gabrielligoski <gabriel.ligoski@gmail.com>
2021-12-01 11:18:17 -0300

    Presentation Page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper w-full h-full">
+            <div className="container">
+                <div className="animated-title">
+                    <div className="text-top text-4xl">
+                        <div>
+                            <span>Você deu mais um passo em direcão ao sucesso</span>
+                            <span>Bem vindo ao CIEE</span>
+                        </div>
+                    </div>
+                    <div className="text-bottom text-3xl">
+                        <div>“Se a oportunidade não bater, construa uma porta”, Milton Berle</div>
+                    </div>
+                </div>
+            </div>
+
+            <ul className="bg-bubbles">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
