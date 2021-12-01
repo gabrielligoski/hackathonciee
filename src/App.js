05bee7315d0ac43cd599a02610f59bc4609b1081
gabrielligoski <gabriel.ligoski@gmail.com>
2021-12-01 12:02:53 -0300

    Presentation Good!

--- src/App.js
+++ src/App.js
@@ -4,32 +4,21 @@ import logo from "./img/logo2.png";
 function App() {
     return (
         <div className="wrapper w-full h-full">
-            <img src={logo} className="w-48 my-6" alt="logo"/>
-            <div className="container">
-                <div className="animated-title">
-                    <div className="text-top text-4xl text-right">
-                        <div className="w-full">
-                            <span>Você deu mais um passo em direcão ao sucesso</span>
-                            <span>Bem vindo ao CIEE</span>
-                        </div>
+            <img src={logo} className="z-50 absolute m-16 w-48" alt="logo"/>
+            {/*<img src={ceci} className="z-50 absolute bottom-0 right-0 m-16 w-64" alt="ceci"/>*/}
+            <div className="animated-title">
+                <div className="text-top text-4xl text-right">
+                    <div className="w-full">
+                        <span>Você deu mais um passo em direcão ao sucesso</span>
+                        <span>Bem vindo ao CIEE</span>
                     </div>
-                    <div className="text-bottom text-3xl">
-                        <div>“Se a oportunidade não bater, construa uma porta”,<br/> Milton Berle</div>
-                    </div>
+                </div>
+                <div className="text-bottom text-3xl">
+                    <div>“Se a oportunidade não bater, construa uma porta”,<br/> Milton Berle</div>
                 </div>
             </div>
 
             <ul className="bg-bubbles">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
                 <li></li>
                 <li></li>
                 <li></li>
